2ec92ac1176eaffcfc7b21a09a9895e8fbda7428
Soyoun Park <soyounpark@Soyouns-MacBook-Pro.local>
2024-04-30 18:25:05 -0700

    Update the code to make a figure

--- code/genus_analysis.R
+++ code/genus_analysis.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(broom)
+library(ggtext)
 
 shared <- read_tsv('raw_data/baxter.subsample.shared',
          col_types = cols(Group=col_character(),
@@ -33,6 +35,40 @@ composite <- inner_join(shared, taxonomy, by='otu') %>%
   select(-count) %>% 
   inner_join(., metadata, by='group')
 
+sig_genera <- composite %>% 
+  nest(data=-taxonomy) %>% 
+  mutate(test=map(.x=data, ~wilcox.test(rel_abund~srn, data=.x) %>% tidy)) %>% 
+  unnest(test) %>% 
+  mutate(p.adjust=p.adjust(p.value, method="BH")) %>% 
+  filter(p.adjust<0.05) %>% 
+  select(taxonomy, p.adjust)
 
+composite %>% 
+  inner_join(sig_genera, by='taxonomy') %>%
+  mutate(rel_abund=100*(rel_abund+1/20000),
+         taxonomy=str_replace(taxonomy, '(.*)', '*\\1*'),
+         taxonomy=str_replace(taxonomy, '\\*(.*)_unclassified\\*', 'Unclassified<br>*\\1*'),
+         srn=factor(srn, levels=c(T,F))) %>% 
+  ggplot(aes(x=rel_abund, y=taxonomy, color=srn, fill=srn)) +
+  geom_jitter(position=position_jitterdodge(dodge.width=0.8,
+                                            jitter.width=0.3),
+              shape=21) +
+  stat_summary(fun.data=median_hilow, fun.args=list(conf.int=0.5),
+               geom='pointrange',
+               position=position_dodge(width=0.8),
+               color='black', show.legend=FALSE) +
+  scale_x_log10() +
+  scale_color_manual(NULL,
+                     breaks=c(F,T),
+                     values=c('gray', 'dodgerblue'),
+                     labels=c('Healthy', 'SRN')) +
+  scale_fill_manual(NULL,
+                     breaks=c(F,T),
+                     values=c('gray', 'dodgerblue'),
+                     labels=c('Healthy', 'SRN')) +
+  labs(x='Relative abundance (%)', y=NULL) +
+  theme_classic() +
+  theme(axis.text.y=element_markdown())
+  
 
-
+ggsave('figures/siginificant_genera.tiff', width=6, height=4)
