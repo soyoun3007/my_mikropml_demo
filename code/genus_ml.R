748697c9e6e3eef8cb4b8764a83c1ce28ab6d929
Soyoun Park <soyounpark@Soyouns-MacBook-Pro.local>
2024-05-06 09:15:59 -0700

    Add code for srn genus preprocessing

--- code/genus_ml.R
+++ code/genus_ml.R
@@ -8,7 +8,9 @@ srn_genus_data <- composite %>%
   mutate(srn=if_else(srn, "srn", "healthy")) %>% 
   select(srn, everything())
 
-srn_genus_results <- run_ml(srn_genus_data,
+srn_genus_preprocessed <- preprocess_data(srn_genus_data, outcome_colname='srn')$dat_transformed
+
+srn_genus_results <- run_ml(srn_genus_preprocessed,
        method='glmnet',
        outcome_colname='srn',
        kfold=5, # default
