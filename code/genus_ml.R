6466c04ae93daffdcd9f9d9f61f48cd9605ad61b
Soyoun Park <soyounpark@Soyouns-MacBook-Pro.local>
2024-05-07 11:14:56 -0700

    Update code including performance data

--- code/genus_ml.R
+++ code/genus_ml.R
@@ -10,11 +10,36 @@ srn_genus_data <- composite %>%
 
 srn_genus_preprocessed <- preprocess_data(srn_genus_data, outcome_colname='srn')$dat_transformed
 
-srn_genus_results <- run_ml(srn_genus_preprocessed,
+test_hp <- list(alpha=0,
+                lambda=c(0.1, 1, 2, 3, 4, 5, 10))
+
+seed=19971016
+
+get_srn_genus_results <- function(seed){
+  run_ml(srn_genus_preprocessed,
        method='glmnet',
        outcome_colname='srn',
        kfold=5, # default
        cv_times=100, # default
        training_frac=0.2, # default
-       seed=19971016)
+       hyperparameters=test_hp,
+       seed=seed)
+}
+
+iterative_run_ml_results <- map(c(1,2,3), get_srn_genus_results)
+
+performance <- iterative_run_ml_results %>%
+  map(pluck, 'trained_model') %>%
+  combine_hp_performance()
+
+plot_hp_performance(performance$dat, lambda, AUC)
+
+performance$dat %>% 
+  group_by(alpha, lambda) %>% 
+  summarize(mean_AUC=mean(AUC), .groups='drop') %>% 
+#  top_n(n=1, mean_AUC)
+  ggplot(aes(x=lambda, y=mean_AUC, color=as.character(alpha))) +
+  geom_line()
+
+get_hyperparams_list(srn_genus_preprocessed, 'glmnet')
 
