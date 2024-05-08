f27defe0e021edf680b27fe1be368f43e88624ca
Soyoun Park <soyounpark@Soyouns-MacBook-Pro.local>
2024-05-08 11:43:31 -0700

    Update code using multisession

--- code/genus_ml.R
+++ code/genus_ml.R
@@ -1,5 +1,12 @@
 source('code/genus_process.R')
 library(mikropml)
+library(tictoc)
+library(furrr)
+
+#plan('sequential') # serial processing, not parallel!
+#plan('multicore') # doesn;t work with windows or Rstudio
+#plan('multisession', workers=8)
+plan('multisession')
 
 srn_genus_data <- composite %>% 
   select(group, taxonomy, rel_abund, srn) %>% 
@@ -16,6 +23,7 @@ test_hp <- list(alpha=0,
 seed=19971016
 
 get_srn_genus_results <- function(seed){
+  
   run_ml(srn_genus_preprocessed,
        method='glmnet',
        outcome_colname='srn',
@@ -24,9 +32,12 @@ get_srn_genus_results <- function(seed){
        training_frac=0.2, # default
        hyperparameters=test_hp,
        seed=seed)
+  
 }
 
-iterative_run_ml_results <- map(c(1,2,3), get_srn_genus_results)
+tic()
+iterative_run_ml_results <- map(1:100, get_srn_genus_results)
+toc()
 
 performance <- iterative_run_ml_results %>%
   map(pluck, 'trained_model') %>%
@@ -37,9 +48,10 @@ plot_hp_performance(performance$dat, lambda, AUC)
 performance$dat %>% 
   group_by(alpha, lambda) %>% 
   summarize(mean_AUC=mean(AUC), .groups='drop') %>% 
-#  top_n(n=1, mean_AUC)
-  ggplot(aes(x=lambda, y=mean_AUC, color=as.character(alpha))) +
-  geom_line()
+  top_n(n=3, mean_AUC)
+#  ggplot(aes(x=lambda, y=mean_AUC, color=as.character(alpha))) +
+#  geom_line()
 
 get_hyperparams_list(srn_genus_preprocessed, 'glmnet')
 
+plan("sequential") # set back to single processor at the end of the run
